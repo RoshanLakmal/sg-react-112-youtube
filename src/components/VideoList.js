61f17f03a566f2f26a30ecc6bc2ffbe73178b663
Roshan Elvitigala <roshanrox911@gmail.com>
2020-10-12 22:46:56 +1100

    created vido list component

---
+++ src/components/VideoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoList = props => {
+    //props.videos
+    return (
+        <div>
+            {props.vidoes.length}
+        </div>
+    )
+}
+
+export default VideoList;
