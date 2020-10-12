8bb350d390960724aaef59113b0fa66b75732524
Roshan Elvitigala <roshanrox911@gmail.com>
2020-10-12 22:00:54 +1100

    youtube get request

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyBCVBL6daZC2iHSrer8Ukt3Ow1XsFoIyIo';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
